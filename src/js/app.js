dce5f2a9b3c14337200e8329430ae73b6d2afce7
reddripilll <nikitakochnev95@mail.ru>
2022-08-25 19:39:48 +0300

    add the swiper to cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,42 @@
 import * as flsFunctions from './modules/functions.js';
 import * as script from './modules/script.js';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 
 flsFunctions.isWebp();
 flsFunctions.ibg();
+
+
+let init = false;
+let salePageCards;
+function initSwiperOnMobile() {
+	if (window.innerWidth <= 768) {
+		if (!init) {
+			init = true;
+			salePageCards = new Swiper('.sale-page__cards', {
+				navigation: {
+					// nextEl: '.swiper-button-next',
+					// prevEl: '.swiper-button-prev'
+				},
+				pagination: {
+					el: '.sale-page__pagination',
+					clickable: true,
+					type: 'fraction',
+				},
+				slidesPerView: 1.08,
+				spaceBetween: 16,
+				watchOverflow: true,
+				loop: true,
+				modules: [Navigation, Pagination],
+			});
+		}
+	} else if (init) {
+		salePageCards.destroy();
+		init = false;
+	}
+}
+initSwiperOnMobile();
+window.addEventListener('resize', () => {
+	initSwiperOnMobile();
+	console.log('Norm');
+});
